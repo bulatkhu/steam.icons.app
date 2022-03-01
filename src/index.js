401e63347c7ea1452d2d992dfa162e249ef2c7d0
bukhudai <bulat.khudainazarov@digia.com>
2022-03-01 17:00:31 +0200

    added rateLimiter middleware, changed retrieving of image hash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const urlMetadata = require('url-metadata')
 const checkApiKey = require('./middleware/checkApiKey')
+const rateLimiter = require('./middleware/rateLimiter')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 2000
@@ -8,29 +9,29 @@ const PORT = process.env.PORT || 2000
 const app = express()
 app.use(express.json())
 app.use((err, req, res, _next) => {
-  console.error('Error occurs:', err.stack)
-  res.status(500).send('Something went wrong')
+    console.error('Error occurs:', err.stack)
+    res.status(500).send('Something went wrong')
 })
 
-app.get('/api/item/img/:hashName', checkApiKey, async (req, res) => {
-  try {
-    const { image: url } = await urlMetadata(`https://steamcommunity.com/market/listings/730/${encodeURI(req.params.hashName)}`)
+app.get('/api/item/img/:hashName', rateLimiter, checkApiKey, async (req, res) => {
+    try {
+        const {image: url} = await urlMetadata(`https://steamcommunity.com/market/listings/730/${encodeURI(req.params.hashName)}`)
 
-    const iconHash = url.replace('https://community.cloudflare.steamstatic.com/economy/image/', '').replace('/360fx360f', '')
+        const iconHash = url.split('/')[5]
 
-    if (iconHash.length < 10) {
-      throw 'Invalid item\'s hash'
+        if (iconHash.length < 10) {
+            throw 'Invalid item\'s hash'
+        }
+
+        res.status(200).json({
+            url,
+            iconHash
+        })
+    } catch (e) {
+        res.status(500).json({error: e})
     }
-
-    res.status(200).json({
-      url,
-      iconHash
-    })
-  } catch (e) {
-    res.status(500).json({ error: e })
-  }
 })
 
 const server = app.listen(PORT, () => {
-  console.log('Server started on PORT:', server.address().port)
+    console.log('Server started on PORT:', server.address().port)
 })
