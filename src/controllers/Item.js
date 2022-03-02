95a63ab6380af0a054ae9a918b3d783af4cfb54f
bukhudai <bulat.khudainazarov@digia.com>
2022-03-02 04:54:43 +0200

    added rarity field to the response

---
+++ src/controllers/Item.js
@@ -0,0 +1,35 @@
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+
+const handleImage = async (req, res) => {
+    try {
+        const browser = await puppeteer.launch({ devtools: true });
+        const page = await browser.newPage();
+        await page.goto(`https://steamcommunity.com/market/listings/730/${encodeURI(req.params.hashName)}`);
+        const content = await page.content();
+        await page.close()
+        await browser.close()
+        const $ = cheerio.load(content)
+
+        const url = $('.market_listing_largeimage').find('img').attr('src')
+        const rarity = $('#largeiteminfo_item_type').contents().first().text();
+
+        const iconHash = url.split('/')[5]
+
+        if (iconHash.length < 10) {
+            throw 'Invalid item\'s hash'
+        }
+
+        res.status(200).json({
+            url,
+            iconHash,
+            rarity
+        })
+    } catch (e) {
+        res.status(500).json({error: e.message})
+    }
+}
+
+module.exports = {
+    handleImage
+}
